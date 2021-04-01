b0ac2c81f94c263ee15b4fa02ad44a3b5678dcd1
Kishin125 <72475864+Kishin125@users.noreply.github.com>
2021-04-01 12:37:26 -0500

    a

---
+++ Assets/script/one.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.UI;
+using UnityEngine.UI;
+using UnityEditor;
+
+/// <summary>
+/// On startup, adds event handlers for every Interactable aka button
+/// in the scene that logs the name of the button pressed.
+/// You can similarly add such a listener when adding the buttons dynamically
+/// </summary>
+public class one : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject cube;
+    public GameObject sphere;
+
+
+    void Start()
+    {
+        
+        var allInteractables = GameObject.FindObjectsOfType<Interactable>();
+        foreach (var i in allInteractables)
+        {
+            i.OnClick.AddListener(() => dothis());
+        }
+    }
+
+    void dothis()
+    {
+        cube.transform.position = new Vector3(-1.926f, 0.795f, 2.583f);
+        sphere.transform.position = new Vector3(-1.917f, 0.8f, 3.19f);
+        cube.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        sphere.GetComponent<Rigidbody>().velocity = Vector3.zero;
+    }
+}
---
+++ Assets/script/reset.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.UI;
+
+public class reset : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject buttons;
+    public one thi;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
