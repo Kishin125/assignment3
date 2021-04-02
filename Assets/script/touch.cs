625a55aeef1695609b68bf03e19126dd907c1690
Kishin125 <72475864+Kishin125@users.noreply.github.com>
2021-04-01 21:19:34 -0500

    add in hoop and score(in debug)

---
+++ Assets/script/touch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touch : MonoBehaviour
+{
+    public GameObject ball;
+    public GameObject thing;
+    public int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision other) 
+    {
+        score += 1;
+        Debug.Log("score is: " + score);
+    }
+}
